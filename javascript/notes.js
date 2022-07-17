dacbeab7bffaa3c3c9ae8475200ca4dd757ff408
Andrew Boeddeker <andrewboeddeker@gmail.com>
2022-07-17 08:26:58 -0500

    first commit

---
+++ javascript/notes.js
@@ -0,0 +1,13 @@
+//Number
+//String
+//Boolean - true/false
+//Undefined - actual value taken by variable that is not yet defined (empty value)
+//Null - empty value
+//Symbol (es2015) - 
+//BigInt - larger integers than the Number type can hold
+
+//Dynamic typing - we do not have to manually define the data type, JS has it automatically (value has type, not variable)
+
+let javascriptIsFun = true;
+
+console.log(typeof javascriptIsFun);
